aa4b359426e62ee484e14bee1fac685da2658d4e
Michael Murray <mmurray24@hotmail.com>
2023-02-18 15:26:32 +0000

    updated class visit and testing

--- models/visit.py
+++ models/visit.py
@@ -1,3 +1,7 @@
 class Visit:
-    def __init__(self):
-        pass
+    def __init__(self, user, destination, date, rating, comment, id = None):
+        self.user =user
+        self.destination = destination
+        self.date = date
+        self.rating = rating
+        self.comment = comment
